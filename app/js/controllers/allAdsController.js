3c2b413f67268362d2f5b1155ad582b27ce05fcc
darinnn <georgiev.darin@gmail.com>
2015-01-02 19:10:26 +0200

    refactor service

--- app/js/controllers/allAdsController.js
+++ app/js/controllers/allAdsController.js
@@ -1,16 +1,18 @@
 /**
  * Created by darin on 1/2/2015.
  */
-adsApp.controller('allAdsController', function($scope, $route, $log, adsData) {
-    adsData.getAll()
-        .$promise
-        .then(function (data) {
+adsApp.controller('allAdsController', function($scope, $route, $log, adsDataService) {
+    adsDataService.getAll(function (data) {
             $scope.data = data;
-        }, function (error) {
-            $log.error(error);
-        });
+    });
+        //.$promise
+        //.then(function (data) {
+        //    $scope.data = data;))
+        //}, function (error) {
+        //    $log.error(error);
+        //});
 
-    $scope.reload = function () {
-        $route.reload();
-    }
+    //$scope.reload = function () {
+    //    $route.reload();
+    //}
 });
