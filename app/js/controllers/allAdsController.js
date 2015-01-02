39c992687006c743b83ab3e83cc85e679361434c
darinnn <georgiev.darin@gmail.com>
2015-01-03 01:13:39 +0200

    add filter forms

--- app/js/controllers/allAdsController.js
+++ app/js/controllers/allAdsController.js
@@ -2,8 +2,15 @@
  * Created by darin on 1/2/2015.
  */
 adsApp.controller('allAdsController', function($scope, $route, $log, adsDataService) {
-    adsDataService.getAll(function (data) {
-            $scope.data = data;
+    adsDataService.getAllAds(function (resp) {
+            $scope.data = resp;
+    });
+
+    adsDataService.getAllTowns(function (resp) {
+        $scope.towns = resp;
+    });
+    adsDataService.getAllCategories(function (resp) {
+        $scope.categories = resp;
     });
         //.$promise
         //.then(function (data) {
--- app/js/services/adsData.js
+++ app/js/services/adsData.js
@@ -3,17 +3,39 @@
  */
 adsApp.factory('adsDataService', function ($http, $log) {
     return {
-        getAll: function(success){
-            var pageIndex=1;
-            $http({method: 'GET', url: 'http://softuni-ads.azurewebsites.net/api/ads?pagesize=10&startpage=' + pageIndex})
-                .success(function(data, status, headers, config){
+        getAllAds: function (success) {
+            var pageIndex = 1;
+            $http({
+                method: 'GET',
+                url: 'http://softuni-ads.azurewebsites.net/api/ads?pagesize=10&startpage=' + pageIndex
+            })
+                .success(function (data, status, headers, config) {
                     success(data);
                 })
-                .error(function(data, status, headers, config){
+                .error(function (data, status, headers, config) {
+                    $log.warn(data);
+                })
+
+        },
+        getAllTowns: function (success) {
+            $http({method: 'GET', url: 'http://softuni-ads.azurewebsites.net/api/towns'})
+                .success(function (data, status, headers, config) {
+                    success(data);
+                })
+                .error(function (data, status, headers, config) {
+                    $log.warn(data);
+                })
+
+        },
+        getAllCategories: function (success) {
+            $http({method: 'GET', url: 'http://softuni-ads.azurewebsites.net/api/categories'})
+                .success(function (data, status, headers, config) {
+                    success(data);
+                })
+                .error(function (data, status, headers, config) {
                     $log.warn(data);
                 })
 
         }
-
     }
 });
