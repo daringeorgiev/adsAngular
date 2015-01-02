7e9ed84c5c83dbbe890d4afcd470f440422b60a4
darinnn <georgiev.darin@gmail.com>
2015-01-02 05:45:30 +0200

    Add login and register forms

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,11 @@
 var app = angular.module('adsApp', ['ngResource', 'ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider.when('/login', {
+            templateUrl: 'templates/login.html',
+            controller: 'loginController'
+        });
+        $routeProvider.when('/register', {
+            templateUrl: 'templates/register.html',
+            controller: 'registerController'
+        });
+    });
